375d18a2057ffc7892d8715fc3b17c1dabf75eb8
AlissaStegall <alissaistegall@gmail.com>
2023-04-24 15:29:24 -0700

    weekly challenges week 1

--- ChallengesWithTestsMark8.Tests/ChallengesSet01Tests.cs
+++ ChallengesWithTestsMark8.Tests/ChallengesSet01Tests.cs
@@ -133,7 +133,7 @@ namespace ChallengesWithTestsMark8.Tests
             ChallengesSet01 challenger = new ChallengesSet01();
 
             // Act
-            string actual = challenger.GetGreeting(personName);
+            string actual = ChallengesSet01.GetGreeting(personName);
 
             // Assert
             Assert.Equal(expectedGreeting, actual);
@@ -146,7 +146,7 @@ namespace ChallengesWithTestsMark8.Tests
             ChallengesSet01 challenger = new ChallengesSet01();
 
             // Act
-            string actual = challenger.GetHey();
+            string actual = ChallengesSet01.GetHey();
 
             // Assert
             Assert.Equal("HEY!", actual);
